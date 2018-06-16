dc4572461727737f95582be13a29490fce95ba55
darkNautic <blue.darknautic@gmail.com>
2018-06-16 12:23:18 -0500

    Changing Project Struture

---
+++ src/main/java/org/codeinmotion/MongoConnectApplication.java
@@ -0,0 +1,12 @@
+package org.codeinmotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoConnectApplication.class, args);
+	}
+}
